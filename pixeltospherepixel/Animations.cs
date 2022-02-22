737e8b8305d9f78ce31e417881b2b2a3991f1ea6
Aaron <aaron.dossantosfr@gmail.com>
2022-02-22 17:19:46 +0100

    Ajoutez des fichiers projet.

---
+++ pixeltospherepixel/Animations.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+
+namespace pixeltospherepixel
+{
+    public static class Animations
+    {
+        public static void ColorAnimationOBJ(Color from, Color to, TimeSpan dur, DependencyObject obj)
+        {
+            ColorAnimation colorChangeAnimation = new ColorAnimation();
+            colorChangeAnimation.From = from;
+            colorChangeAnimation.To = to;
+            colorChangeAnimation.Duration = dur;
+
+            PropertyPath colorTargetPath = new PropertyPath("(Rectangle.Fill).(SolidColorBrush.Color)");
+            Storyboard CellBackgroundChangeStory = new Storyboard();
+            Storyboard.SetTarget(colorChangeAnimation, obj);
+            Storyboard.SetTargetProperty(colorChangeAnimation, colorTargetPath);
+            CellBackgroundChangeStory.Children.Add(colorChangeAnimation);
+            CellBackgroundChangeStory.Begin();
+        }
+        public static void MarginToMargin(Thickness from, Thickness to, TimeSpan dur, FrameworkElement obj)
+        {
+            var a = new ThicknessAnimation
+            {
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.Stop,
+                Duration = dur
+            };
+
+            var storyboard = new Storyboard();
+
+            storyboard.Children.Add(a);
+
+            Storyboard.SetTarget(a, obj);
+            Storyboard.SetTargetProperty(a, new PropertyPath(FrameworkElement.MarginProperty));
+            storyboard.Completed += delegate { obj.Margin = to; };
+            storyboard.Begin();
+        }
+
+        public static void BlurRadius(int to, int from, TimeSpan speed, FrameworkElement obj)
+        {
+            var a = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.Stop,
+                Duration = speed
+            };
+
+            var storyboard = new Storyboard();
+
+            storyboard.Children.Add(a);
+
+            Storyboard.SetTarget(a, obj);
+            Storyboard.SetTargetProperty(a, new PropertyPath("Effect.Radius"));
+            storyboard.Completed += delegate { obj.Effect = new BlurEffect() { Radius = to }; };
+            storyboard.Begin();
+        }
+
+        public static void ShadowOpacity(double to, double from, TimeSpan speed, FrameworkElement obj)
+        {
+            var a = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.Stop,
+                Duration = speed
+            };
+
+            var storyboard = new Storyboard();
+
+            storyboard.Children.Add(a);
+
+            Storyboard.SetTarget(a, obj);
+            Storyboard.SetTargetProperty(a, new PropertyPath("Effect.Opacity"));
+            storyboard.Completed += delegate { (obj.Effect as DropShadowEffect).Opacity = to; };
+            storyboard.Begin();
+        }
+
+        public static void Opacity(double to, double from, TimeSpan speed, FrameworkElement obj)
+        {
+            var a = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.Stop,
+                Duration = speed
+            };
+
+            var storyboard = new Storyboard();
+
+            storyboard.Children.Add(a);
+
+            Storyboard.SetTarget(a, obj);
+            Storyboard.SetTargetProperty(a, new PropertyPath(FrameworkElement.OpacityProperty));
+            storyboard.Completed += delegate { obj.Opacity = to; if (to == 0) { obj.Visibility = Visibility.Hidden; } };
+            storyboard.Begin();
+        }
+
+        public static void Stroke(double to, double from, TimeSpan speed, FrameworkElement obj)
+        {
+            var a = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.Stop,
+                Duration = speed
+            };
+
+            var storyboard = new Storyboard();
+
+            storyboard.Children.Add(a);
+
+            Storyboard.SetTarget(a, obj);
+            Storyboard.SetTargetProperty(a, new PropertyPath(System.Windows.Shapes.Ellipse.StrokeThicknessProperty));
+            storyboard.Completed += delegate { };
+            storyboard.Begin();
+        }
+
+        public static void Width(double from, double to, TimeSpan speed, FrameworkElement obj)
+        {
+            var a = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.Stop,
+                Duration = speed
+            };
+
+            var storyboard = new Storyboard();
+
+            storyboard.Children.Add(a);
+
+            Storyboard.SetTarget(a, obj);
+            Storyboard.SetTargetProperty(a, new PropertyPath(FrameworkElement.WidthProperty));
+            storyboard.Completed += delegate { obj.Width = to; };
+            storyboard.Begin();
+        }
+
+        public static void Height(double from, double to, TimeSpan speed, FrameworkElement obj)
+        {
+            var a = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                FillBehavior = FillBehavior.Stop,
+                Duration = speed
+            };
+
+            var storyboard = new Storyboard();
+
+            storyboard.Children.Add(a);
+
+            Storyboard.SetTarget(a, obj);
+            Storyboard.SetTargetProperty(a, new PropertyPath(FrameworkElement.HeightProperty));
+            storyboard.Completed += delegate { obj.Height = to; };
+            storyboard.Begin();
+        }
+
+
+        public static void Scale(object sender, double from, double to, TimeSpan speed)
+        {
+            Storyboard storyboard = new Storyboard();
+
+            ScaleTransform scale = new ScaleTransform(1.0, 1.0);
+            (sender as UIElement).RenderTransformOrigin = new System.Windows.Point(0.5, 0.5);
+            (sender as UIElement).RenderTransform = scale;
+
+            DoubleAnimation growAnimation = new DoubleAnimation();
+            DoubleAnimation growAnimation2 = new DoubleAnimation();
+            growAnimation.Duration = speed;
+            growAnimation2.Duration = speed;
+            growAnimation.From = from;
+            growAnimation2.From = from;
+            growAnimation.To = to;
+            growAnimation2.To = to;
+            storyboard.Children.Add(growAnimation);
+            storyboard.Children.Add(growAnimation2);
+
+            Storyboard.SetTargetProperty(growAnimation, new PropertyPath("RenderTransform.ScaleX"));
+            Storyboard.SetTargetProperty(growAnimation2, new PropertyPath("RenderTransform.ScaleY"));
+            Storyboard.SetTarget(growAnimation, (sender as UIElement));
+            Storyboard.SetTarget(growAnimation2, (sender as UIElement));
+
+            storyboard.Begin();
+        }
+
+        public static void Angle(object sender, double from, double to, TimeSpan speed)
+        {
+            Storyboard storyboard = new Storyboard();
+
+            RotateTransform rotate = new RotateTransform();
+            (sender as UIElement).RenderTransformOrigin = new System.Windows.Point(0.5, 0.5);
+            (sender as UIElement).RenderTransform = rotate;
+
+            DoubleAnimation growAnimation = new DoubleAnimation();
+            growAnimation.Duration = speed;
+            growAnimation.From = from;
+            growAnimation.To = to;
+            storyboard.Children.Add(growAnimation);
+
+            Storyboard.SetTargetProperty(growAnimation, new PropertyPath("RenderTransform.Angle"));
+            Storyboard.SetTarget(growAnimation, (sender as UIElement));
+
+            storyboard.Begin();
+        }
+
+    }
+}
---
+++ pixeltospherepixel/MainWindow.xaml.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Point = System.Windows.Point;
+
+namespace pixeltospherepixel
+{
+    public partial class MainWindow : Window
+    {
+        private Point? lastCenterPositionOnTarget;
+        private Point? lastMousePositionOnTarget;
+        private Point? lastDragPoint;
+
+        private object _isMoving;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            scrollViewer.ScrollChanged += OnScrollViewerScrollChanged;
+            scrollViewer.MouseLeftButtonUp += OnMouseLeftButtonUp;
+            scrollViewer.PreviewMouseRightButtonUp += OnMouseLeftButtonUp;
+            scrollViewer.PreviewMouseWheel += OnPreviewMouseWheel;
+
+            scrollViewer.PreviewMouseRightButtonDown += OnMouseLeftButtonDown;
+            scrollViewer.MouseMove += OnMouseMove;
+
+            AskFile.Visibility = Visibility.Visible;
+        }
+
+        void OnMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (lastDragPoint.HasValue)
+            {
+                Point posNow = e.GetPosition(scrollViewer);
+
+                double dX = posNow.X - lastDragPoint.Value.X;
+                double dY = posNow.Y - lastDragPoint.Value.Y;
+
+                lastDragPoint = posNow;
+
+                scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset - dX);
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - dY);
+            }
+        }
+
+        void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var mousePos = e.GetPosition(scrollViewer);
+            if (mousePos.X <= scrollViewer.ViewportWidth && mousePos.Y <
+                scrollViewer.ViewportHeight)
+            {
+                scrollViewer.Cursor = System.Windows.Input.Cursors.SizeAll;
+                lastDragPoint = mousePos;
+                Mouse.Capture(scrollViewer);
+            }
+        }
+
+        double zoomAmount = 1;
+        void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double oldZoom = zoomAmount;
+            double v = 0;
+            if (Keyboard.IsKeyDown(Key.LeftCtrl)) v = 0.5;
+            if (Keyboard.IsKeyDown(Key.LeftShift)) v = 0.5;
+
+            if (v == 0) return;
+            lastMousePositionOnTarget = Mouse.GetPosition(grid);
+
+            if (e.Delta > 0)
+                zoomAmount += v;
+            if (e.Delta < 0)
+                zoomAmount -= v;
+
+            scaleTransform.ScaleX = zoomAmount;
+            scaleTransform.ScaleY = zoomAmount;
+            var centerOfViewport = new Point(scrollViewer.ViewportWidth / 2,
+                              scrollViewer.ViewportHeight / 2);
+            lastCenterPositionOnTarget = scrollViewer.TranslatePoint(centerOfViewport, grid);
+            Console.WriteLine("XX");
+
+            e.Handled = true;
+        }
+
+        void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            scrollViewer.Cursor = System.Windows.Input.Cursors.Arrow;
+            scrollViewer.ReleaseMouseCapture();
+            lastDragPoint = null;
+        }
+
+        void OnScrollViewerScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.ExtentHeightChange != 0 || e.ExtentWidthChange != 0)
+            {
+                Point? targetBefore = null;
+                Point? targetNow = null;
+
+                if (!lastMousePositionOnTarget.HasValue)
+                {
+                    if (lastCenterPositionOnTarget.HasValue)
+                    {
+                        var centerOfViewport = new Point(scrollViewer.ViewportWidth / 2,
+                                                         scrollViewer.ViewportHeight / 2);
+                        Point centerOfTargetNow =
+                              scrollViewer.TranslatePoint(centerOfViewport, grid);
+
+                        targetBefore = lastCenterPositionOnTarget;
+                        targetNow = centerOfTargetNow;
+                    }
+                }
+                else
+                {
+                    targetBefore = lastMousePositionOnTarget;
+                    targetNow = Mouse.GetPosition(grid);
+
+                    lastMousePositionOnTarget = null;
+                }
+
+                if (targetBefore.HasValue)
+                {
+                    double dXInTargetPixels = targetNow.Value.X - targetBefore.Value.X;
+                    double dYInTargetPixels = targetNow.Value.Y - targetBefore.Value.Y;
+
+                    double multiplicatorX = e.ExtentWidth / grid.Width;
+                    double multiplicatorY = e.ExtentHeight / grid.Height;
+
+                    double newOffsetX = scrollViewer.HorizontalOffset -
+                                        dXInTargetPixels * multiplicatorX;
+                    double newOffsetY = scrollViewer.VerticalOffset -
+                                        dYInTargetPixels * multiplicatorY;
+
+                    if (double.IsNaN(newOffsetX) || double.IsNaN(newOffsetY))
+                    {
+                        return;
+                    }
+
+                    scrollViewer.ScrollToHorizontalOffset(newOffsetX);
+                    scrollViewer.ScrollToVerticalOffset(newOffsetY);
+                }
+            }
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Bitmap bitmap = FixedSize(new Bitmap(openFileDialog.FileName), 256, 256) as Bitmap;
+                new Thread(() =>
+                {
+                    System.Windows.Application.Current.Dispatcher.Invoke(new Action(() =>
+                    {
+                        Animations.Opacity(0, 1, new TimeSpan(0, 0, 0, 0, 300), AskFile);
+                        //this.Width = bitmap.Width;
+                        //this.Height =  bitmap.Height;
+                    }));
+
+                    Thread.Sleep(300);
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        System.Windows.Application.Current.Dispatcher.Invoke(new Action(() =>
+                        {
+                            StackPanel stackPanel = new StackPanel() { Orientation = System.Windows.Controls.Orientation.Horizontal, Background = new SolidColorBrush(System.Windows.Media.Colors.White), Height = 2 };
+                            SP.Children.Add(stackPanel);
+                            for (int x = 0; x < bitmap.Width; x++)
+                            {
+                                var color = bitmap.GetPixel(x, y);
+
+                                Ellipse ellipse = new Ellipse() { Height = 2, Width = 2, Fill = new SolidColorBrush(System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B)) };
+                                stackPanel.Children.Add(ellipse);
+                            }
+                        }));
+                    }
+
+                }).Start();
+
+            }
+        }
+
+        static System.Drawing.Image FixedSize(System.Drawing.Image imgPhoto, int Width, int Height)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((Width -
+                              (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((Height -
+                              (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(Width, Height,
+                              System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution,
+                             imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(System.Drawing.Color.White);
+            grPhoto.InterpolationMode =
+                    InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new System.Drawing.Rectangle(destX, destY, destWidth, destHeight),
+                new System.Drawing.Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+    }
+}
